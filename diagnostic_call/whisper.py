13cd39e60916734674621d010d73b808aac56a73
Clovis Lechien <clovis.lechien@epita.fr>
2024-10-12 14:07:27 +0200

    feat(whisper): testing

--- diagnostic_call/whisper.py
+++ diagnostic_call/whisper.py
@@ -1,5 +1,29 @@
-import whisper
-model = whisper.load_model('large')
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+from pydub import AudioSegment
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
 
 
 def get_transcribe(audio: str, language: str = 'en'):
@@ -7,6 +31,7 @@ def get_transcribe(audio: str, language: str = 'en'):
 
 
 if __name__ == "__main__":
-    result = get_transcribe(audio='test-audio.mp3',)
+    audio_file = "test-audio.mp3"
+    result = get_transcribe(audio=audio_file)
     print('-'*50)
     print(result.get('text', ''))
