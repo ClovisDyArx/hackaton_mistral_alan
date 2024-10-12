af628657c3a55178cb1f68af58ec3c148bd48a7a
Clovis Lechien <clovis.lechien@epita.fr>
2024-10-12 14:04:31 +0200

    feat(whisper): testing

--- diagnostic_call/whisper.py
+++ diagnostic_call/whisper.py
@@ -1,31 +1,12 @@
-import torch
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
-from pydub import AudioSegment
+import whisper
+model = whisper.load_model('large')
 
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+def get_transcribe(audio: str, language: str = 'en'):
+    return model.transcribe(audio=audio, language=language, verbose=True)
 
-model_id = "openai/whisper-large-v3"
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-)
-model.to(device)
-
-processor = AutoProcessor.from_pretrained(model_id)
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device,
-)
-
-new_audio_file = "test-audio.mp3"
-result = model.transcribe(new_audio_file)
-
-print(f' The text in audio file: \n {result["text"]}')
+if __name__ == "__main__":
+    result = get_transcribe(audio='test-audio.mp3',)
+    print('-'*50)
+    print(result.get('text', ''))
